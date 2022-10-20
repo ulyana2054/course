902470c8e71644a2f6145d2781d67176bc6faab3
chashka <chashka@localhost>
2022-10-20 15:49:00 +0300

    First commit
    our commit

---
+++ FirstProject/src/code/Class.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ FirstProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chashka
+ *
+ */
+module FirstProject {
+}

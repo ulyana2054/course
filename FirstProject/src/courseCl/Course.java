54c0d5243700424e9dd723ea6fb388c950e713b3
chashka <chashka@localhost>
2022-10-23 23:21:34 +0300

    homework

---
+++ FirstProject/src/courseCl/Course.java
@@ -0,0 +1,42 @@
+package courseCl;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Course {
+	protected int id;
+	protected String title;
+	protected int duration;
+	protected String branch;
+	protected List<String> tags = new ArrayList<String>();
+	
+	Course(int id, String title, int duration, String branch){
+		this.id = id;
+		this.title = title;
+		this.duration = duration;
+		this.branch = branch;
+	}
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public int getDuration() {
+		return this.duration;
+	}
+	
+	public String getBranch() {
+		return this.branch;
+	}
+	
+	public void addTag(String tag) {
+		tags.add(tag);
+	}
+	
+	public void removeTag(int pos) {
+		tags.remove(pos);
+	}
+	
+}
